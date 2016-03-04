21249bbf56936d134438b0d4de76baf4c0ea17e3
Alejandra Quintana <alejandra.quintana@gmail.com>
2016-03-03 22:10:54 -0300

    Ejercicio 1 modificado.

--- 1.rb
+++ 1.rb
@@ -5,14 +5,12 @@ b = [:azul, :rojo, :amarillo]
 c = ["Tacos", "Quesadillas", "Hamburguesas"]
 # ==> ["Tacos", "Quesadillas", "Hamburguesas"]
 
-require "pp"
-# ==> true
 
-a.zip(b,c).each do |a, b,c|
-	puts "#{a} #{b} #{c}" 
+a.zip(b,c).each do |x|
+	puts x.inspect
 end
-# =1 azul Tacos
-# =2 rojo Quesadillas
-# =3 amarillo Hamburguesas
+# =[1, :azul, "Tacos"]
+# =[2, :rojo, "Quesadillas"]
+# =[3, :amarillo, "Hamburguesas"]
 # ==> [[1, :azul, "Tacos"], [2, :rojo, "Quesadillas"], [3, :amarillo, "Hamburguesas"]]
 
